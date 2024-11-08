70db083d310b2196af75faf9b21ac9b037636fbe
Fefusco <o email do git>
2024-11-08 09:07:48 -0300

    tela login

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent, pathMatch:'full' }, // Rota inicial
+  { path: 'dashboard', component: DashboardComponent },
+];
+
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { ReactiveFormsModule, UntypedFormBuilder, UntypedFormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-login', 
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './login.component.html', 
+  styleUrls: ['./login.component.css']  // Adicione esta linha
+})
+export class LoginComponent implements OnInit {
+  editForm: UntypedFormGroup;
+  
+  constructor(private router: Router, private formBuilder: UntypedFormBuilder) {}
+
+  ngOnInit() {
+    this.editForm = this.formBuilder.group({
+      username: [''], 
+      password: ['']
+    });
+  }
+
+  onSubmit() {
+    console.log(this.editForm);
+    if (this.editForm.controls['username'].value === 'admin' && this.editForm.controls['password'].value === 'admin') {
+      this.router.navigate(['/dashboard']);
+    } else {
+      console.log("teste");
+      alert('Credenciais inválidas');
+    }
+  }
+}
